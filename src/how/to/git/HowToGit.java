350151b4a57378eb699937230848badde03d25df
Amsalk99 <43138821+Amsalk99@users.noreply.github.com>
2018-10-08 14:39:13 +0200

    init

---
+++ src/how/to/git/HowToGit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package how.to.git;
+
+/**
+ *
+ * @author Amsal
+ */
+public class HowToGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
